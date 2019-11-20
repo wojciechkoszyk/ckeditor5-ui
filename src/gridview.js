d519875d4c34bdb32e056b3290ee9b2bc4169e86
Marek Lewandowski <m.lewandowski@cksource.com>
2019-11-20 16:30:46 +0100

    Internal: Introduced GridView class.

---
+++ src/gridview.js
@@ -0,0 +1,17 @@
+/**
+ * @license Copyright (c) 2003-2019, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+/**
+ * @module ui/grid
+ */
+
+import View from './view';
+
+/**
+ * A grid of nested views.
+ *
+ * @extends module:ui/view~View
+ */
+export default class GridView extends View {}
