0214d308865a2149293a3b06c55c67d615312c43
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2020-02-10 10:39:10 +0100

    Tests: Added more editors and more complex content to the test.

--- tests/manual/tickets/6200/1.js
+++ tests/manual/tickets/6200/1.js
@@ -6,45 +6,69 @@
 /* globals window, document, console:false */
 
 import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import InlineEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
+import BalloonEditor from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
 import ArticlePluginSet from '@ckeditor/ckeditor5-core/tests/_utils/articlepluginset';
 
-ClassicEditor
-	.create( document.querySelector( '#editor' ), {
-		plugins: [ ArticlePluginSet ],
-		toolbar: [
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'|',
-			'bulletedList',
-			'numberedList',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'|',
-			'undo',
-			'redo'
-		],
-		image: {
-			toolbar: [ 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' ]
-		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells'
-			]
-		}
-	} )
-	.then( editor => {
-		window.editor = editor;
+const EDITOR_CONFIG = {
+	plugins: [ ArticlePluginSet ],
+	toolbar: [
+		'heading',
+		'|',
+		'bold',
+		'italic',
+		'link',
+		'|',
+		'bulletedList',
+		'numberedList',
+		'blockQuote',
+		'insertTable',
+		'mediaEmbed',
+		'|',
+		'undo',
+		'redo'
+	],
+	image: {
+		toolbar: [ 'imageStyle:full', 'imageStyle:side', '|', 'imageTextAlternative' ]
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn',
+			'tableRow',
+			'mergeTableCells'
+		]
+	}
+};
 
-		document.querySelector( '#btn-readonly' ).addEventListener( 'click', () => {
-			editor.isReadOnly = !editor.isReadOnly;
-		} );
+ClassicEditor
+	.create( document.querySelector( '#editor-classic' ), EDITOR_CONFIG )
+	.then( editor => {
+		window.classicEditor = editor;
 	} )
 	.catch( err => {
 		console.error( err.stack );
 	} );
+
+InlineEditor
+	.create( document.querySelector( '#editor-inline' ), EDITOR_CONFIG )
+	.then( editor => {
+		window.inlineEditor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
+
+BalloonEditor
+	.create( document.querySelector( '#editor-balloon' ), EDITOR_CONFIG )
+	.then( editor => {
+		window.balloonEditor = editor;
+	} )
+	.catch( err => {
+		console.error( err.stack );
+	} );
+
+document.querySelector( '#btn-readonly' ).addEventListener( 'click', () => {
+	window.classicEditor.isReadOnly = !window.classicEditor.isReadOnly;
+	window.inlineEditor.isReadOnly = !window.inlineEditor.isReadOnly;
+	window.balloonEditor.isReadOnly = !window.balloonEditor.isReadOnly;
+} );
